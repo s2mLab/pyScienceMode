005d8e15fb1849b16046046c50047ff103afee5e
ArseneBA <arsene.baert@etu.emse.fr>
2022-06-23 16:25:54 -0400

    Updated example

--- example.py
+++ example.py
@@ -3,103 +3,105 @@ from pyScienceMode2 import Stimulator as St
 # Import Channel class
 from pyScienceMode2 import Channel as Ch
 
-from time import time
 from time import sleep
-while 1:
-    # Create a list of channel
-    list_channels = []
 
-    # Create all channels possible
-    channel_1 = Ch.Channel(mode='Single', no_channel=1, amplitude=2, pulse_width=100, stimulation_interval=10,
-                           inter_pulse_interval=10, name='Biceps')
+# Create a list of channel
+list_channels = []
 
-    channel_2 = Ch.Channel()
-    channel_2.set_mode('Single')
-    channel_2.set_no_channel(2)
-    channel_2.set_amplitude(2)
-    channel_2.set_pulse_width(100)
-    channel_2.set_stimulation_interval(10)
-    channel_2.set_inter_pulse_interval(10)
-    channel_2.set_name('Triceps')
+# Create all channels possible
+channel_1 = Ch.Channel(mode='Single', no_channel=1, amplitude=5, pulse_width=100, inter_pulse_interval=10,
+                       name='Biceps')
 
-    channel_3 = Ch.Channel('Single', 3, 2, 100, 11, 10)
-    channel_4 = Ch.Channel('Single', 4, 2, 100, 10, 10)
-    channel_5 = Ch.Channel('Single', 5, 2, 100, 10, 10)
-    channel_6 = Ch.Channel('Single', 6, 2, 100, 10, 10)
-    channel_7 = Ch.Channel('Single', 7, 2, 100, 10, 10)
-    channel_8 = Ch.Channel('Single', 8, 2, 100, 10, 10)
+channel_2 = Ch.Channel()
+channel_2.set_mode('Single')
+channel_2.set_no_channel(2)
+channel_2.set_amplitude(2)
+channel_2.set_pulse_width(100)
+channel_2.set_inter_pulse_interval(10)
+channel_2.set_name('Triceps')
 
-    # Choose which channel will be used, Warning channel 2 and 4 are not working in our case
-    list_channels.append(channel_1)
-    list_channels.append(channel_3)
-    list_channels.append(channel_5)
-    list_channels.append(channel_6)
-    list_channels.append(channel_7)
-    list_channels.append(channel_8)
+channel_3 = Ch.Channel('Single', 3, 2, 100, 10)
+channel_4 = Ch.Channel('Single', 4, 2, 100, 10)
+channel_5 = Ch.Channel('Single', 5, 2, 100, 10)
+channel_6 = Ch.Channel('Single', 6, 2, 100, 10)
+channel_7 = Ch.Channel('Single', 7, 2, 100, 10)
+channel_8 = Ch.Channel('Single', 8, 2, 100, 10)
 
-    # Create our object Stimulator
-    stimulator = St.Stimulator(list_channels, 'COM4')
+# Choose which channel will be used
+list_channels.append(channel_1)
+list_channels.append(channel_3)
+list_channels.append(channel_5)
+list_channels.append(channel_6)
+list_channels.append(channel_7)
+list_channels.append(channel_8)
 
-    # Show the log, by default True if called, to deactivate : stimulator.show_log(False)
-    stimulator.show_log()
-    # stimulator.show_com()
+# Create our object Stimulator
+stimulator = St.Stimulator(list_channels=list_channels, stimulation_interval=1000, port_path='COM4')
 
-    # print("     Initialisation")
-    # Start the stimulation with the given parameters
-    # Possible to stop the stimulation after a given time with start_stimulation(time), if used, start_stimulation must be
-    # used again.
+# Display log, communication or/and watchdog, by default True if called, to deactivate : stimulator.show_log(False)
+stimulator.show_log()
+# stimulator.show_com()
+# stimulator.show_watchdog()
 
-    stimulator.init_channel()
-    # n = 0
-    # while n < 10:
-    #     print(n % 2)
-    #     if n % 2 == 0:
-    #         stimulator.start_stimulation(stimulation_duration=1, wait_rehastim_response=False)
-    #     else:
-    #         stimulator.start_stimulation(stimulation_duration=1)
-    #     sleep(3)
-    #     n += 1
+"""
+Initialise the channels given.
+It is possible to modify the list of channels and the stimulation interval
+"""
+stimulator.init_channel()
+# stimulator.init_channel(stimulation_interval=200, list_channels=nv_list_channel)
 
-    stimulator.start_stimulation(stimulation_duration=1)
-    # Modify some parameters,
-    list_channels[2].amplitude = 10
-    list_channels[3].amplitude = 15
+"""
+Start the stimulation.
+It is possible to :
+- Give a time after which the stimulation will be stopped but not disconnected.
+- Update the parameters of the channel by giving a new list of channels. The channel given must have been 
+  initialised first.
+"""
+stimulator.start_stimulation()
+# stimulator.start_stimulation(stimulation_duration=10, upd_list_channels=nw_list_channel)
 
-    # print("     Wait for 5 sec")
-    # Wait a given time in seconds
-    sleep(1)
+# Modify some parameters,
+list_channels[2].amplitude = 10
+list_channels[3].amplitude = 15
 
-    # print("     Update signal")
-    # Update the parameters of the stimulation
-    stimulator.start_stimulation(upd_list_channels=list_channels)
+# Wait a given time in seconds
+sleep(1)
 
-    # Wait a given time in seconds
-    # sleep(5)
-    #
-    # # Stop the stimulation
-    # print("     Stop stimulation")
-    stimulator.stop_stimulation()
+# Update the parameters of the stimulation
+stimulator.start_stimulation(upd_list_channels=list_channels)
 
-    # print("     Wait 5 sec")
-    sleep(1)
+# Wait a given time in seconds
+sleep(5)
 
-    # print("     Restart stimulation for 10 sec")
-    # Disconnect the Rehastim, stop sending watchdog
-    stimulator.start_stimulation(2)
+"""
+Stop the stimulation. But does not disconnect the Pc and the Rehastim.
+"""
+stimulator.stop_stimulation()
 
-    # print("     Disconnect")
-    stimulator.disconnect()
+"""
+Restart a stimulation with the same parameter for 2 seconds.
+"""
+stimulator.start_stimulation(stimulation_duration=2)
 
-    # print("     Sleep 2 sec")
-    sleep(1)
+"""
+The method init_channel must be called to update the stimulation interval (period).
+"""
+stimulator.init_channel(stimulation_interval=10)
+stimulator.start_stimulation(stimulation_duration=2)
 
-    # print("     Init")
-    stimulator.init_channel()
+"""
+To disconnect the computer and the Rehastim, use the disconnect method.
+"""
+stimulator.disconnect()
 
-    # print("     Start for 5 sec")
-    # It is possible to restart a stimulation, the program automatically connect to the Rehastim
-    stimulator.start_stimulation(2, list_channels, False)
+"""
+After a disconnection, init_channel must be called.  
+"""
+stimulator.init_channel(stimulation_interval=15)
+stimulator.start_stimulation(2, list_channels)
+stimulator.disconnect()
 
-    # Stimulator.disconnect() must be called in order to finish the program and stop send watchdog.
-    stimulator.disconnect()
-    stimulator.close_port()
+"""
+close_port method closes the serial port.
+"""
+stimulator.close_port()
