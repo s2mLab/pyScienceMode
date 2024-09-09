52f0bb43db58309db7fa8f0aeb43794e937bd8f4
Kevin CO <kevinco@umontreal.ca>
2024-09-09 17:37:29 -0400

    feat(reha2 example): making stimulation range independent from loop

--- Examples/handbike_rehastim2.py
+++ Examples/handbike_rehastim2.py
@@ -39,23 +39,27 @@ class HandCycling2:
         ]
 
         # Set the intensity for each muscles
-        self.intensity = {"biceps_r": 28,
-                          "triceps_r": 26,
-                          "delt_ant_r": 25,
-                          "delt_post_r": 30,
-                          "biceps_l": 26,
-                          "triceps_l": 28,
-                          "delt_ant_l": 34,
-                          "delt_post_l": 36}
+        self.intensity = {
+            "biceps_r": 10,
+            "triceps_r": 10,
+            "delt_ant_r": 5,
+            "delt_post_r": 5,
+            "biceps_l": 10,
+            "triceps_l": 10,
+            "delt_ant_l": 5,
+            "delt_post_l": 5,
+        }
 
-        self.channel_number = {"biceps_r": 2,
-                               "triceps_r": 1,
-                               "delt_ant_r": 3,
-                               "delt_post_r": 4,
-                               "biceps_l": 6,
-                               "triceps_l": 5,
-                               "delt_ant_l": 7,
-                               "delt_post_l": 8}
+        self.channel_number = {
+            "biceps_r": 2,
+            "triceps_r": 1,
+            "delt_ant_r": 3,
+            "delt_post_r": 4,
+            "biceps_l": 6,
+            "triceps_l": 5,
+            "delt_ant_l": 7,
+            "delt_post_l": 8,
+        }
 
         self.stimulation_state = {
             "triceps_r": False,
@@ -69,25 +73,32 @@ class HandCycling2:
         }
 
         # Create our object Stimulator
-        self.stimulator = St(port="COM7", show_log=True)
+        self.stimulator = St(port="COM7", show_log=False)
         self.stimulator.init_channel(
-            stimulation_interval=22, list_channels=self.list_channels, low_frequency_factor=0
+            stimulation_interval=22,
+            list_channels=self.list_channels,
+            low_frequency_factor=0,
         )
-
         self.angle = 0
 
-        self.stimulation_range = {"biceps_r": [220, 10],
-                                  "triceps_r": [20, 180],
-                                  "delt_ant_r": [20, 180],
-                                  "delt_post_r": [220, 10],
-                                  "biceps_l": [40, 190],
-                                  "triceps_l": [200, 360],
-                                  "delt_ant_l": [200, 360],
-                                  "delt_post_l": [40, 190]}
+        self.stimulation_range = {
+            "biceps_r": [220, 10],
+            "triceps_r": [20, 180],
+            "delt_ant_r": [20, 180],
+            "delt_post_r": [220, 10],
+            "biceps_l": [40, 190],
+            "triceps_l": [200, 360],
+            "delt_ant_l": [200, 360],
+            "delt_post_l": [40, 190],
+        }
 
         self.stim_condition = {}
         for key in self.stimulation_range.keys():
-            self.stim_condition[key] = 1 if self.stimulation_range[key][0] < self.stimulation_range[key][1] else 0
+            self.stim_condition[key] = (
+                1
+                if self.stimulation_range[key][0] < self.stimulation_range[key][1]
+                else 0
+            )
 
         # This is to initialize the encoder for the pedal angle
         local_system = nidaqmx.system.System.local()
@@ -115,7 +126,6 @@ class HandCycling2:
             self.origin = self.task.read() - self.min_voltage
             self.angle_coeff = 360 / (max_voltage - self.min_voltage)
             self.actual_voltage = None
-            self.stimulation_state = None
 
     def get_angle(self):
         voltage = self.task.read() - self.min_voltage
@@ -131,144 +141,52 @@ class HandCycling2:
 
         while 1:
             self.get_angle()
-            print(self.angle)
-
+            intensity_to_update = False
             for key in self.stimulation_range.keys():
                 if self.stim_condition[key] == 0:
-                    if self.stimulation_range[key][0] <= self.angle < self.stimulation_range[key][1] and self.stimulation_state[key] == 0:
-                        self.stimulation_state[key] = 1
-                        self.list_channels[self.channel_number[key]-1].set_amplitude(self.intensity[key])
-                    elif self.stimulation_range[key][0] > self.angle or self.angle > self.stimulation_range[key][1] and self.stimulation_state[key] == 1:
-                        self.stimulation_state[key] = 0
-                        self.list_channels[self.channel_number[key] - 1].set_amplitude(0)
+                    if (
+                        self.stimulation_range[key][0] <= self.angle <= 360
+                        or 0 <= self.angle <= self.stimulation_range[key][1]
+                    ) and self.stimulation_state[key] is False:
+                        self.stimulation_state[key] = True
+                        self.list_channels[self.channel_number[key] - 1].set_amplitude(
+                            self.intensity[key]
+                        )
+                        intensity_to_update = True
+                    elif (
+                        self.stimulation_range[key][1]
+                        < self.angle
+                        < self.stimulation_range[key][0]
+                        and self.stimulation_state[key] is True
+                    ):
+                        self.stimulation_state[key] = False
+                        self.list_channels[self.channel_number[key] - 1].set_amplitude(
+                            0
+                        )
+                        intensity_to_update = True
                 else:
-                    if self.stimulation_range[key][0] < self.angle or self.angle < self.stimulation_range[key][1] and self.stimulation_state[key] == 0:
-                        self.stimulation_state[key] = 1
-                        self.list_channels[self.channel_number[key] - 1].set_amplitude(self.intensity[key])
-                    elif self.stimulation_range[key][0] > self.angle or self.angle > self.stimulation_range[key][1] and self.stimulation_state[key] == 1:
-                        self.stimulation_state[key] = 0
-                        self.list_channels[self.channel_number[key] - 1].set_amplitude(self.intensity[key])
-
-            # # Phase 1 / From 0° to 10°
-            # # right biceps and deltoid posterior activated
-            # if 0 <= self.angle < 10 and self.stimulation_state["Biceps_r"] + self.stimulation_state["Delt_post_r"] \
-            #         + self.stimulation_state["Triceps_l"] + self.stimulation_state["Delt_ant_l"] != 2:
-            #     for list_chan in self.list_channels:
-            #         list_chan.set_amplitude(0)
-            #     self.list_channels[1].set_amplitude(self.biceps_r_intensity)
-            #     self.list_channels[3].set_amplitude(self.delt_post_r_intensity)
-            #
-            #     self.stimulator.start_stimulation(
-            #         upd_list_channels=self.list_channels
-            #     )
-            #     for stim_state in self.stimulation_state:
-            #         self.stimulation_state[stim_state] = False
-            #     self.stimulation_state["Biceps_r"] = True
-            #     self.stimulation_state["Delt_post_r"] = True
-            #
-            # # Phase 2 / From 10° to 20°
-            # # no muscle activated
-            # elif 10 <= self.angle < 20 and all(a == 0 for a in self.stimulation_state.values()) is False:
-            #     for list_chan in self.list_channels:
-            #         list_chan.set_amplitude(0)
-            #     self.stimulator.start_stimulation(upd_list_channels=self.list_channels)
-            #     for stim_state in self.stimulation_state:
-            #         self.stimulation_state[stim_state] = False
-            #
-            # # Phase 3 / From 20° to 40°
-            # # right triceps and deltoid anterior activated
-            # elif 30 <= self.angle < 40 and self.stimulation_state["Triceps_r"] + self.stimulation_state[
-            #     "Delt_ant_r"] != 2:
-            #     for list_chan in self.list_channels:
-            #         list_chan.set_amplitude(0)
-            #     self.list_channels[0].set_amplitude(self.triceps_r_intensity)
-            #     self.list_channels[2].set_amplitude(self.delt_ant_r_intensity)
-            #
-            #     self.stimulator.start_stimulation(upd_list_channels=self.list_channels)
-            #     for stim_state in self.stimulation_state:
-            #         self.stimulation_state[stim_state] = False
-            #     self.stimulation_state["Triceps_r"] = True
-            #     self.stimulation_state["Delt_ant_r"] = True
-            #
-            # # Phase 4 / From 40° to 180°
-            # # right triceps, right deltoid anterior, left biceps and left deltoid posterior activated
-            # elif 40 <= self.angle < 200 and self.stimulation_state["Triceps_r"] + self.stimulation_state[
-            #     "Delt_ant_r"] \
-            #         + self.stimulation_state["Biceps_l"] + self.stimulation_state["Delt_post_l"] != 4:
-            #     for list_chan in self.list_channels:
-            #         list_chan.set_amplitude(0)
-            #     self.list_channels[0].set_amplitude(self.triceps_r_intensity)
-            #     self.list_channels[2].set_amplitude(self.delt_ant_r_intensity)
-            #     self.list_channels[5].set_amplitude(self.biceps_l_intensity)
-            #     self.list_channels[7].set_amplitude(self.delt_post_l_intensity)
-            #
-            #     self.stimulator.start_stimulation(upd_list_channels=self.list_channels)
-            #     for stim_state in self.stimulation_state:
-            #         self.stimulation_state[stim_state] = False
-            #     self.stimulation_state["Triceps_r"] = True
-            #     self.stimulation_state["Delt_ant_r"] = True
-            #     self.stimulation_state["Biceps_l"] = True
-            #     self.stimulation_state["Delt_post_l"] = True
-            #
-            # # Phase 5 / From 180° to 190°
-            # # left biceps and left deltoid posterior activated
-            # elif 180 <= self.angle < 190 and self.stimulation_state["Triceps_r"] + self.stimulation_state[
-            #     "Delt_ant_r"] \
-            #         + self.stimulation_state["Biceps_l"] + self.stimulation_state["Delt_post_l"] != 2:
-            #     for list_chan in self.list_channels:
-            #         list_chan.set_amplitude(0)
-            #     self.list_channels[5].set_amplitude(self.biceps_l_intensity)
-            #     self.list_channels[7].set_amplitude(self.delt_post_l_intensity)
-            #
-            #     self.stimulator.start_stimulation(upd_list_channels=self.list_channels)
-            #     for stim_state in self.stimulation_state:
-            #         self.stimulation_state[stim_state] = False
-            #     self.stimulation_state["Biceps_l"] = True
-            #     self.stimulation_state["Delt_post_l"] = True
-            #
-            # # Phase 6 / From 190° to 200°
-            # # no muscle activated
-            # elif 190 <= self.angle < 200 and all(a == 0 for a in self.stimulation_state.values()) is False:
-            #     for list_chan in self.list_channels:
-            #         list_chan.set_amplitude(0)
-            #     self.stimulator.start_stimulation(upd_list_channels=self.list_channels)
-            #     for stim_state in self.stimulation_state:
-            #         self.stimulation_state[stim_state] = False
-            #
-            # # Phase 7 / From 200° to 220°
-            # # left triceps and left deltoid anterior activated
-            # elif 210 <= self.angle < 240 and self.stimulation_state["Triceps_l"] + self.stimulation_state[
-            #     "Delt_ant_l"] != 2:
-            #     for list_chan in self.list_channels:
-            #         list_chan.set_amplitude(0)
-            #     self.list_channels[4].set_amplitude(self.triceps_l_intensity)
-            #     self.list_channels[6].set_amplitude(self.delt_ant_l_intensity)
-            #
-            #     self.stimulator.start_stimulation(upd_list_channels=self.list_channels)
-            #     for stim_state in self.stimulation_state:
-            #         self.stimulation_state[stim_state] = False
-            #     self.stimulation_state["Triceps_l"] = True
-            #     self.stimulation_state["Delt_ant_l"] = True
-            #
-            # # Phase 8 / From 220° to 360°
-            # # right biceps, right deltoid posterior, left triceps and left deltoid anterior activated
-            # elif 220 <= self.angle < 350 and self.stimulation_state["Biceps_r"] + self.stimulation_state[
-            #     "Delt_post_r"] \
-            #         + self.stimulation_state["Triceps_l"] + self.stimulation_state["Delt_ant_l"] != 4:
-            #     for list_chan in self.list_channels:
-            #         list_chan.set_amplitude(0)
-            #     self.list_channels[1].set_amplitude(self.biceps_r_intensity)
-            #     self.list_channels[3].set_amplitude(self.delt_post_r_intensity)
-            #     self.list_channels[4].set_amplitude(self.triceps_l_intensity)
-            #     self.list_channels[6].set_amplitude(self.delt_ant_l_intensity)
-            #
-            #     self.stimulator.start_stimulation(upd_list_channels=self.list_channels)
-            #     for stim_state in self.stimulation_state:
-            #         self.stimulation_state[stim_state] = False
-            #     self.stimulation_state["Biceps_r"] = True
-            #     self.stimulation_state["Delt_post_r"] = True
-            #     self.stimulation_state["Triceps_l"] = True
-            #     self.stimulation_state["Delt_ant_l"] = True
+                    if (
+                        self.stimulation_range[key][0]
+                        <= self.angle
+                        <= self.stimulation_range[key][1]
+                        and self.stimulation_state[key] is False
+                    ):
+                        self.stimulation_state[key] = True
+                        self.list_channels[self.channel_number[key] - 1].set_amplitude(
+                            self.intensity[key]
+                        )
+                        intensity_to_update = True
+                    elif (
+                        self.stimulation_range[key][0] > self.angle
+                        or self.angle > self.stimulation_range[key][1]
+                    ) and self.stimulation_state[key] is True:
+                        self.stimulation_state[key] = False
+                        self.list_channels[self.channel_number[key] - 1].set_amplitude(
+                            0
+                        )
+                        intensity_to_update = True
+            if intensity_to_update:
+                self.stimulator.start_stimulation(upd_list_channels=self.list_channels)
 
             time.sleep(0.001)
 
