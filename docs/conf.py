6bb47fbd25c7c44bc74e1e11ae639483e6b4a03f
Kevin CO <kevinco@umontreal.ca>
2024-11-15 11:18:47 -0500

    ver

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2023, Baert, Ceglia, Co, Faresin"
 author = "Baert, Ceglia, Co, Faresin "
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.3"
+release = "1.1.4"
 
 
 # -- General configuration ---------------------------------------------------
